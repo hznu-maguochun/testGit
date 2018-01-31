f9a989a31b9f370cc8cf8d97316824a36b544aa6
Ma Guochun <maguochun@163.com>
2018-01-31 20:19:30 +0800

    First time

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+ 
+    @IBOutlet weak var ttt: UIButton!
+    
+    @IBAction func output(){
+        print(ttt.titleLabel?.text! as Any)
+        print("Good");
+    }
 
 }
 
